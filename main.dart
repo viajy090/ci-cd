66acc776f56fc7672bab97eedbe334110d58db3c
vijay <vijaycherry567@gmail.com>
2025-03-11 19:01:31 +0530

    added all needed files to the repository

---
+++ main.dart
@@ -0,0 +1,19 @@
+// void main() {
+//   var count = 0;
+//   while (count < 3) {
+//     print('hello matthew');
+//     count++;
+//   }
+// }
+
+void main() {
+  String char = 'a';
+  switch (char) {
+    case 'a':
+      print('superb');
+      break;
+    case 'b':
+      print('avg');
+      break;
+  }
+}
